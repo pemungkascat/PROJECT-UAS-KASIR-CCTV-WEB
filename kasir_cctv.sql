ad6adf62e8ac5b7244d44a74de4c75b55b76108e
pemungkascat <250119005@mhs.udb.ac.id>
2026-01-17 00:31:39 +0700

    Update kasir_cctv.sql

--- kasir_cctv.sql
+++ kasir_cctv.sql
@@ -1,21 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Jan 16, 2026 at 05:25 PM
--- Server version: 8.4.3
--- PHP Version: 8.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `kasir_cctv`
